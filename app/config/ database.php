2c90ccd5999eaa291e99b5968faae36398d4cda2
Laissy <laissy@backstagedigital.com.br>
2025-04-25 09:07:58 -0300

    teste

---
+++ app/config/ database.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = '127.0.0.1';
+$db   = 'sistema_doacao';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
